6918a88026149d39acddd261e56aab2c4c192a26
felipemendes <f.ribeiromendes@gmail.com>
2018-10-26 10:12:52 -0300

    Implement view code. No storyboard anymore

---
+++ README.md
@@ -0,0 +1,1 @@
+# PurAí iOS App
--- purai-ios-app/ViewController.swift
+++ purai-ios-app/ViewController.swift
@@ -8,13 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+     
+        navigationItem.title = "Próximos eventos"
+        collectionView?.backgroundColor = UIColor.white
+        
+        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
     }
-
-
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = UIColor.red
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 200)
+    }
 }
 
