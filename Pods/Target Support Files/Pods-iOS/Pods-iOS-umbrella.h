af63f9b4099a2f10c59b0a3dcea8019092017b4f
felipemendes <f.ribeiromendes@gmail.com>
2019-01-12 15:55:13 -0200

    Remove old files

--- Pods/Target Support Files/Pods-iOS/Pods-iOS-umbrella.h
+++ Pods/Target Support Files/Pods-iOS/Pods-iOS-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_purai_ios_appVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_purai_ios_appVersionString[];
+FOUNDATION_EXPORT double Pods_iOSVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_iOSVersionString[];
 
